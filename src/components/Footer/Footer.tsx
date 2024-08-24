de4fccab81fa2a42e8f3eb1b8908084d6aa4ba23
Golam Rabbani <golamrabbani9373@gmail.com>
2024-08-24 23:14:58 +0600

    footer complete

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,23 +1,205 @@
+import {Link} from 'react-router-dom';
+import {BiLogoFacebook, BiLogoTwitter, BiLogoInstagram, BiLogoLinkedin} from 'react-icons/bi';
 const Footer = () => {
 	return (
-		<div>
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius aspernatur fuga quisquam et
-			assumenda hic reiciendis ratione illum deleniti eos nulla nostrum debitis velit similique
-			excepturi vel libero blanditiis error pariatur nemo porro, quo accusamus nihil provident!
-			Voluptatum suscipit accusamus minus veritatis a architecto ab ut dolorum corporis corrupti
-			consequuntur porro sapiente, magni, esse numquam. Debitis, libero at? Provident ducimus
-			mollitia quo nesciunt culpa eum itaque harum ipsa aliquam, earum nulla animi omnis veritatis
-			expedita quibusdam optio consequuntur impedit cum pariatur, incidunt vel architecto dolorem?
-			Totam alias magnam provident perferendis est cupiditate eius, modi excepturi voluptatem
-			aliquam neque voluptatibus itaque molestias, hic rerum sint maiores a beatae aspernatur, non
-			eligendi? Exercitationem sequi sint eos provident esse vero necessitatibus ducimus velit
-			eaque, nostrum tempore totam quo repellat aspernatur, accusantium natus ex? Impedit porro,
-			modi aspernatur rem non perferendis fuga est adipisci similique dolore ducimus nesciunt, nulla
-			fugit exercitationem doloremque atque consectetur a tenetur illum! Hic ullam dicta at
-			accusamus ipsa consectetur ducimus debitis! Id, pariatur praesentium. Eligendi totam,
-			voluptatibus dolorum fuga odit, neque suscipit recusandae harum accusantium enim est ipsa quas
-			sit ullam in dignissimos, adipisci saepe rerum quam voluptas laboriosam cumque sint? Quam
-			voluptatem dolor nesciunt veritatis deserunt iure molestiae.
+		<div className="bg-black overflow-hidden">
+			<div className="container mx-auto px-[40px] md:px-[120px]">
+				{/* !footer top */}
+				<div className="footer-top pt-[100px]  pb-[50px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-center gap-7">
+					<div>
+						<h2 className="font-extrabold uppercase text-white text-base pb-4">INFORMATION</h2>
+						<Link
+							to={'/'}
+							className="block text-sm text-[#9B9B97] hover:text-white transition-all hover:translate-x-3 duration-500 pb-2"
+						>
+							Delivery Information
+						</Link>
+						<Link
+							to={'/'}
+							className="block text-sm text-[#9B9B97] hover:text-white transition-all hover:ml-4 duration-500 pb-2"
+						>
+							Terms & Conditions
+						</Link>
+						<Link
+							to={'/'}
+							className="block text-sm text-[#9B9B97] hover:text-white transition-all hover:ml-4 duration-500 pb-2"
+						>
+							Contact
+						</Link>
+						<Link
+							to={'/'}
+							className="block text-sm text-[#9B9B97] hover:text-white transition-all hover:ml-4 duration-500 pb-2"
+						>
+							Returns
+						</Link>
+					</div>
+					<div>
+						<h2 className="font-extrabold uppercase text-white text-base pb-4">My account</h2>
+						<Link
+							to={'/'}
+							className="block text-sm text-[#9B9B97] hover:text-white transition-all hover:translate-x-3 duration-500 pb-2"
+						>
+							My Account
+						</Link>
+						<Link
+							to={'/'}
+							className="block text-sm text-[#9B9B97] hover:text-white transition-all hover:ml-4 duration-500 pb-2"
+						>
+							Wishlist
+						</Link>
+						<Link
+							to={'/'}
+							className="block text-sm text-[#9B9B97] hover:text-white transition-all hover:ml-4 duration-500 pb-2"
+						>
+							Privacy Policy
+						</Link>
+						<Link
+							to={'/'}
+							className="block text-sm text-[#9B9B97] hover:text-white transition-all hover:ml-4 duration-500 pb-2"
+						>
+							Frequently Questions
+						</Link>
+						<Link
+							to={'/'}
+							className="block text-sm text-[#9B9B97] hover:text-white transition-all hover:ml-4 duration-500 pb-2"
+						>
+							Order History
+						</Link>
+					</div>
+					<div>
+						<h2 className="font-extrabold uppercase text-white text-base pb-4">Pages</h2>
+						<Link
+							to={'/'}
+							className="block text-sm text-[#9B9B97] hover:text-white transition-all hover:translate-x-3 duration-500 pb-2"
+						>
+							Home
+						</Link>
+						<Link
+							to={'/shop'}
+							className="block text-sm text-[#9B9B97] hover:text-white transition-all hover:ml-4 duration-500 pb-2"
+						>
+							Shop
+						</Link>
+						<Link
+							to={'/about-us'}
+							className="block text-sm text-[#9B9B97] hover:text-white transition-all hover:ml-4 duration-500 pb-2"
+						>
+							About Us
+						</Link>
+						<Link
+							to={'/manage-products'}
+							className="block text-sm text-[#9B9B97] hover:text-white transition-all hover:ml-4 duration-500 pb-2"
+						>
+							Product-management
+						</Link>
+						<Link
+							to={'/shop/cart'}
+							className="block text-sm text-[#9B9B97] hover:text-white transition-all hover:ml-4 duration-500 pb-2"
+						>
+							Cart
+						</Link>
+					</div>
+					<div>
+						<h2 className="font-extrabold uppercase text-white text-base pb-4">ABOUT US</h2>
+						<Link
+							to={'/'}
+							className="block text-sm text-[#9B9B97] hover:text-white transition-all hover:translate-x-3 duration-500 pb-2"
+						>
+							Welcome to BikeIst Bike Rentals, your ultimate solution for high-quality bikes and
+							seamless rental experiences
+						</Link>
+						<Link
+							to={'/'}
+							className="block text-sm text-[#9B9B97] hover:text-white transition-all hover:ml-4 duration-500 pb-2"
+						>
+							Address: Your address goes here.
+						</Link>
+						<Link
+							to={'/'}
+							className="block text-sm text-[#9B9B97] hover:text-white transition-all hover:ml-4 duration-500 pb-2"
+						>
+							Email:demo@example.com
+						</Link>
+					</div>
+				</div>
+			</div>
+			<div className=" bg-slate-400  h-[1px]"></div>
+			{/* Footer Middle */}
+			<div className="container mx-auto px-[40px] md:px-[120px]">
+				<div className="grid grid-cols-1 md:grid-cols-2 items-center justify-center">
+					{/* Social */}
+					<div className="py-[60px] xl:pl-36">
+						<h2 className="font-extrabold uppercase text-white text-base pb-4">FOLLOW US</h2>
+						<div className="social list-none inline-flex gap-4 ">
+							<li className="">
+								<Link
+									to={'/'}
+									className="flex justify-center items-center w-10 h-10 rounded-full bg-[#2f3239] text-white hover:bg-white hover:text-primary transition-all duration-500"
+								>
+									<BiLogoFacebook className="w-5 h-5" />
+								</Link>
+							</li>
+							<li className=" ">
+								<Link
+									to={'/'}
+									className="flex justify-center items-center w-10 h-10 rounded-full bg-[#2f3239] text-white hover:bg-white hover:text-primary transition-all duration-500"
+								>
+									<BiLogoTwitter className="w-5 h-5" />
+								</Link>
+							</li>
+							<li className=" ">
+								<Link
+									to={'/'}
+									className="flex justify-center items-center w-10 h-10 rounded-full bg-[#2f3239] text-white hover:bg-white hover:text-primary transition-all duration-500"
+								>
+									<BiLogoInstagram className="w-5 h-5" />
+								</Link>
+							</li>
+							<li className=" ">
+								<Link
+									to={'/'}
+									className="flex justify-center items-center w-10 h-10 rounded-full bg-[#2f3239] text-white hover:bg-white hover:text-primary transition-all duration-500"
+								>
+									<BiLogoLinkedin className="w-5 h-5" />
+								</Link>
+							</li>
+						</div>
+					</div>
+					{/* Newsletter */}
+					<div className="">
+						<h2 className="font-extrabold uppercase text-white text-base pb-4">
+							DON'T MISS OUT ON THE LATEST
+						</h2>
+
+						<div className="flex mb-5">
+							<input
+								type="text"
+								className="w-full h-[50px] border rounded-l border-[#9B9B97] outline-none px-4 text-white bg-transparent focus:border-primary transition-all duration-500"
+								placeholder="Enter your email"
+							/>
+							<button className="btn bg-primary hover:bg-secondary rounded-none h-[50px] px-2 uppercase text-white font-bold">
+								Subscribe
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className=" bg-slate-400  h-[1px]"></div>
+			<div className="container mx-auto px-[40px] md:px-[120px]">
+				<div className="flex flex-wrap justify-between items-center ">
+					<div className="copyright py-[20px]">
+						<h2 className="text-white uppercase text-sm">
+							© {new Date().getFullYear()} Bikeist. Made by{' '}
+							<Link to={`https://webrabbani.web.app/`} className="text-primary">
+								Golam rabbani
+							</Link>
+						</h2>
+					</div>
+					<div className="company">
+						<img src="https://i.ibb.co/340xmC5/payment.png" alt="" />
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
