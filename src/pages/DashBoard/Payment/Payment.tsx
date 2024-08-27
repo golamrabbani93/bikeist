298a4a5eaaefc68d780a9441fedd6eb3791935e4
Golam Rabbani <golamrabbani9373@gmail.com>
2024-08-27 19:03:06 +0600

    payment modal complete

--- src/pages/DashBoard/Payment/Payment.tsx
+++ src/pages/DashBoard/Payment/Payment.tsx
@@ -1,5 +1,11 @@
+import {ConfigProvider, Modal, theme} from 'antd';
 import moment from 'moment';
-import {useLocation} from 'react-router-dom';
+import {useState} from 'react';
+import {FieldValues, SubmitHandler} from 'react-hook-form';
+import {useLocation, useNavigate} from 'react-router-dom';
+import {useAppSelector} from '../../../redux/hooks';
+import {getCurrentTheme} from '../../../redux/features/theme/themeSlice';
+import {TBike} from '../../../types';
 
 const Payment = () => {
 	const location = useLocation();
@@ -7,7 +13,6 @@ const Payment = () => {
 	const bikeDetails = bikeData?.bikeData;
 	const startTime = bikeData?.startTime;
 	const modifyDate = moment(new Date(startTime)).format(' Do MMMM YYYY, h:mm A');
-	console.log('🚀🚀: Payment -> modifyDate', modifyDate);
 
 	return (
 		<div className="min-h-screen bg-gradient-to-br from-primary to-gray-900 flex items-center justify-center p-4">
@@ -39,11 +44,55 @@ const Payment = () => {
 				</div>
 
 				{/* Pay Button */}
-				<button className="w-full py-3 bg-primary text-white rounded-lg shadow-lg hover:bg-secondary focus:ring-4 focus:ring-secondary focus:outline-none transition duration-300">
+				{/* <button className="w-full py-3 bg-primary text-white rounded-lg shadow-lg hover:bg-secondary focus:ring-4 focus:ring-secondary focus:outline-none transition duration-300">
 					Pay $100
 				</button>
+                 */}
+				<PaymentModal />
 			</div>
 		</div>
+	);
+};
+const PaymentModal = ({bikeData}: {bikeData: TBike}) => {
+	const [open, setOpen] = useState(false);
+
+	// *theme Management
+	const selectedTheme = useAppSelector(getCurrentTheme);
+	const showModal = () => {
+		setOpen(true);
+	};
+
+	const handleCancel = () => {
+		setOpen(false);
+	};
+
+	const lightTheme = {};
+
+	const darkTheme = {
+		algorithm: theme.darkAlgorithm,
+	};
+
+	// * payment mathod start
+
+	return (
+		<>
+			<button
+				onClick={showModal}
+				className="mt-8 px-6 py-3 bg-[#e2211c] text-white font-bold uppercase rounded-lg hover:bg-red-700 transition duration-300 w-full"
+			>
+				Pay $100
+			</button>
+			<ConfigProvider theme={selectedTheme === 'light' ? lightTheme : darkTheme}>
+				<Modal
+					open={open}
+					title={`${bikeData?.name}Bike Book Now`}
+					onCancel={handleCancel}
+					footer={[]}
+				>
+					<div className="mt-2"></div>
+				</Modal>
+			</ConfigProvider>
+		</>
 	);
 };
 
