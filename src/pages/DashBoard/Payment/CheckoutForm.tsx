228d8d53cc8fccd001f3a9c744ff1d23536b5a1e
Golam Rabbani <golamrabbani9373@gmail.com>
2024-08-30 11:43:32 +0600

    change stripe url

--- src/pages/DashBoard/Payment/CheckoutForm.tsx
+++ src/pages/DashBoard/Payment/CheckoutForm.tsx
@@ -49,11 +49,14 @@ const CheckoutForm: React.FC<TProps> = ({
 	useEffect(() => {
 		const createPaymentIntent = async () => {
 			try {
-				const res = await fetch('http://localhost:5000/api/create-payment-intent', {
-					method: 'POST',
-					headers: {'Content-Type': 'application/json'},
-					body: JSON.stringify({price: amount}),
-				});
+				const res = await fetch(
+					'https://bike-rental-reservation-system-backend.vercel.app/api/create-payment-intent',
+					{
+						method: 'POST',
+						headers: {'Content-Type': 'application/json'},
+						body: JSON.stringify({price: amount}),
+					},
+				);
 
 				const data = await res.json();
 				setClientSecret(data.clientSecret);
